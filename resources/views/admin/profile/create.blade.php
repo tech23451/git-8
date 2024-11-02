c3048bc621de1352cf1e37a4cd878aeee29350ce
aeri.mitsumaru <ox.a11s1001@gmail.com>
2024-11-02 06:05:45 +0000

    Laravel10

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="uft-8">
-        <title>Laravel10</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Laravel10-2</title>
     </head>
     <body>
         <h1>練習</h1>
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE httml>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta name="csrf-token" contact="{{ csrf_token() }}">
+
+        <title>@yield('title')</title>
+
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+        
+        <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+        <link href=
