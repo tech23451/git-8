897a7631329ba3a98412125256df83af261967d2
aeri.mitsumaru <ox.a11s1001@gmail.com>
2024-11-03 04:11:05 +0000

    Larabel11

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -15,7 +15,7 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
         
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/profile.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
