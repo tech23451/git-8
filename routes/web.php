c53f61ec4715aab93eff4fb9118e096257af0649
aeri.mitsumaru <ox.a11s1001@gmail.com>
2024-11-04 04:33:01 +0000

    Laravel12

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
 Route::controller(AAAcontroller::class)->group(function(){
@@ -28,8 +28,8 @@ Route::controller(AAAcontroller::class)->group(function(){
 
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->group(function(){
-    Route::get('profile/create/', 'add');
-    Route::get('profile/edit/', 'edit');   
+    Route::get('profile/create/', 'add')->middleware('auth');
+    Route::get('profile/edit/', 'edit')->middleware('auth');   
 });
     
 Auth::routes();
